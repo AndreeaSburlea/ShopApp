83111b76270a799d6e063150996ac7e9fbeaf960
VivianaSara <viviana.mesaros@fortech.ro>
2022-05-26 09:49:36 +0300

    commit skeleton

---
+++ ShopApp/ShopApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShopApp
+//
+//  Created by Andreea Sburlea on 25.05.2022.
+//
+
+import UIKit
+import FirebaseDatabase
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        let ref = Database.database().reference()
+
+    }
+}
